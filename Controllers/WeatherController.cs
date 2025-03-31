4fa2ab320ef68100d348cd0681b85271e18f255f
alyssadw328 <alyssadw328@yahoo.com>
2025-03-31 03:51:56 -0400

    upload

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StormChasersGroupProject2.Models;
+
+namespace StormChasersGroupProject2.Controllers
+{
+    public class WeatherController : Controller
+    {
+        //here we will retrieve/fetch the weather data
+        public IActionResult Index()
+        {
+            //adding an example, just until we have the live data attached
+            var weather = new WeatherModel
+            {
+                City = "Spartanburg",
+                Temperature = 60.5,
+                HumidityPercent = 94,
+                WindSpeed = 7,
+                PrecipType = "Rain",
+                PrecipPercent = 17,
+                WeatherIcon = "rainyicon.png"
+
+            };
+            return View(weather);
+        }
+    }
+}
---
+++ Models/WeatherModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StormChasersGroupProject2.Models
+{
+    public class WeatherModel
+    {
+        public string City { get; set; }
+        public double Temperature { get; set; }
+        public double HumidityPercent { get; set; }
+        public double WindSpeed { get; set; }
+        public string PrecipType { get; set; }
+        public double PrecipPercent { get; set; }
+        public string? WeatherIcon { get; set; }
+        //this is for the weather icon/image for each weather condition
+    }
+}
