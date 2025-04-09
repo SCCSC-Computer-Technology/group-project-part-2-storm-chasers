0bb8fe804a6e2cbaeb153c1216256864ca0ffab0
Colin Gaffney <colin.gaffney@proton.me>
2025-04-09 17:49:33 -0400

    Added search and default
    Added a search bar to find different cities. An empty search defaults to Spartanburg.

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -18,8 +18,38 @@ namespace StormChasersGroupProject2.Controllers
         }
 
         // Index action to fetch weather data from WeatherAPI.com
-        public async Task<IActionResult> Index(string city = "Spartanburg")
+        public async Task<IActionResult> Index()
         {
+            string city = "Spartanburg";
+            // Define the API endpoint and the request URL
+            var url = $"https://api.weatherapi.com/v1/current.json?key={_apiKey}&q={city}";
+
+            // Make the HTTP request and retrieve the weather data as a string
+            var response = await _httpClient.GetStringAsync(url);
+
+            // Deserialize the JSON response into a dynamic object
+            var weatherData = JsonConvert.DeserializeObject<dynamic>(response);
+
+            // Create a WeatherModel instance and populate it with data from the API response
+            var weather = new WeatherModel
+            {
+                City = weatherData.location.name,
+                Temperature = weatherData.current.temp_f,
+                HumidityPercent = weatherData.current.humidity,
+                WindSpeed = weatherData.current.wind_mph,
+                PrecipType = weatherData.current.condition.text, // Rain, Sunny, etc.
+                PrecipPercent = weatherData.current.precip_in,
+                WeatherIcon = weatherData.current.condition.icon
+            };
+
+            // Pass the populated WeatherModel to the view
+            return View(weather);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string city)
+        {
+            if (city == null) city = "Spartanburg";
+
             // Define the API endpoint and the request URL
             var url = $"https://api.weatherapi.com/v1/current.json?key={_apiKey}&q={city}";
 
--- Models/WeatherModel.cs
+++ Models/WeatherModel.cs
@@ -2,11 +2,11 @@
 {
     public class WeatherModel
     {
-        public string City { get; set; }
+        public string? City { get; set; }
         public double Temperature { get; set; }
         public double HumidityPercent { get; set; }
         public double WindSpeed { get; set; }
-        public string PrecipType { get; set; }
+        public string? PrecipType { get; set; }
         public double PrecipPercent { get; set; }
         public string? WeatherIcon { get; set; }
         //this is for the weather icon/image for each weather condition
